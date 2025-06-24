b0d98588738a8dd324620b0ae0ac036fdd631829
Azriel09 <jhanmarmel@gmail.com>
2025-06-24 20:19:10 +0800

    minor fix

--- src/components/Home/Card_view/Filtered_cards.tsx
+++ src/components/Home/Card_view/Filtered_cards.tsx
@@ -188,7 +188,7 @@ export default function FilteredCards({
                   strokeLinejoin="round"
                   d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
                 />
-              </svg>
+              </svg>  
             )}
           </button>
         </div>
