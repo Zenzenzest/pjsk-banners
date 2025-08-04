7f5b2a756bac31794d72621cfd4730edc1112252
Azriel09 <jhanmarmel@gmail.com>
2025-08-04 09:19:03 +0800

    comments

--- src/components/Home/Filter_view/Card_grid.tsx
+++ src/components/Home/Filter_view/Card_grid.tsx
@@ -15,9 +15,12 @@ export default function CardGrid({ mode, cardId, cardName }: CardGridPropType) {
         <div className="animate-pulse bg-gray-300 dark:bg-gray-600 aspect-[1.76/1] w-full rounded-lg" />
       )}
       <div className={`${thumbLoader.isLoaded ? "contents" : "hidden"}`}>
+        {/* Treating miku touhou card as if its a bday card */}
+        {/* because no trained image lol wtf */}
+        {/* imagine paying for no trained image */}
         <img
           src={`/images/card_thumbnails/${cardId}${
-            cardId === 1167 || mode === "bd" ? "_bd" : mode === "t" ? "_t" : ""
+            cardId === 1167 || mode === "bd" ? "_bd" : mode === "t" ? "_t" : "" 
           }.webp`}
           className={`h-auto w-full max-w-[300px] ml-auto mr-auto rounded`}
           alt={cardName}
