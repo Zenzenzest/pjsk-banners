f26d7e3fcb88f90bcc3ae8e22c9a2a5c39f8cbfd
Azriel09 <jhanmarmel@gmail.com>
2025-09-19 17:17:41 +0800

    theme adjustment

--- src/components/CardCounter_view/Compact/Compact_container.tsx
+++ src/components/CardCounter_view/Compact/Compact_container.tsx
@@ -1,10 +1,18 @@
+import { useTheme } from "../../../context/Theme_toggle";
 import WebsiteDisclaimer from "../../Nav/Website_disclaimer";
 import CompactBday from "./Bday/Compact_bday";
 import CompactBFes from "./Bloom_fes/Compact_bfes";
 
 export default function CompactContainer() {
+  const { theme } = useTheme();
   return (
-    <div className="w-full flex flex-col h-auto bg-[#101828] overflow-x-hidden ">
+    <div
+      className={`w-full flex flex-col h-auto  ${
+        theme === "dark"
+          ? "bg-[#101828] text-gray-300"
+          : "bg-[#f5f7f9] text-gray-700"
+      } overflow-x-hidden `}
+    >
       <h1 className="text-center text-4xl ">WIP</h1>
       <div className="flex flex-row flex-wrap gap-5 justify-center items-center">
         <CompactBFes />
