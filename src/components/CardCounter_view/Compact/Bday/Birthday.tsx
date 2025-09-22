97959b59be5e52fea7b1b78a0cae495f680e805c
Azriel09 <jhanmarmel@gmail.com>
2025-09-22 12:01:37 +0800

    reusable tab component

--- src/components/CardCounter_view/Compact/Bday/Birthday.tsx
+++ src/components/CardCounter_view/Compact/Bday/Birthday.tsx
@@ -4,10 +4,12 @@ import CanvasTable from "../CanvasTable_container";
 import { grouped, imgHost } from "../../../../constants/common";
 import { useFilteredBatch } from "./useFilterbyBatch";
 import { useTheme } from "../../../../context/Theme_toggle";
+import { TabNavigation } from "../../../Nav/TabNav";
 export default function CompactBday() {
   const { allCards } = useProsekaData();
   const { theme } = useTheme();
   const [batch, setBatch] = useState(4);
+  const tabNames = ["1st", "2nd", "3rd", "4th", "5th"];
   const bdayGrid: [number, number] = [6, 7];
 
   const icons = Array(6)
@@ -35,82 +37,22 @@ export default function CompactBday() {
 
   return (
     <div>
-      <div className="min-h-[90px]">
+      <div className="lg:min-h-[70px]">
         <h1 className="text-center">Birthday / Anniversary</h1>
         <div
-          className={`max-w-[350px] flex mx-auto  border-b ${
+          className={`max-w-[350px] flex flex-row justify-center items-center mx-auto gap-5  border-b ${
             theme === "light" ? "border-gray-200" : "border-gray-700 "
           }`}
         >
-          <button
-            onClick={() => setBatch(1)}
-            className={`px-2 py-3 mr-2 w-1/4 text-sm font-medium relative transition-colors duration-200 ${
-              batch === 1
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            1st Rotation
-            {batch === 1 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-
-          <button
-            onClick={() => setBatch(2)}
-            className={`px-2 py-3 text-sm w-1/4 font-medium relative transition-colors duration-200 ${
-              batch === 2
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            2nd Rotation
-            {batch === 2 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-          <button
-            onClick={() => setBatch(3)}
-            className={`px-2 py-3 text-sm w-1/4 font-medium relative transition-colors duration-200 ${
-              batch === 3
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            3rd Rotation
-            {batch === 3 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-          <button
-            onClick={() => setBatch(4)}
-            className={`px-2 py-3 text-sm w-1/4 font-medium relative transition-colors duration-200 ${
-              batch === 4
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            4th Rotation
-            {batch === 4 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
+          {tabNames.map((tab, i) => (
+            <TabNavigation
+              currentTab={batch}
+              setCurrentTab={setBatch}
+              tab={tab}
+              n={i + 1}
+              tabs={tabNames}
+            />
+          ))}
         </div>
       </div>
       <CanvasTable
