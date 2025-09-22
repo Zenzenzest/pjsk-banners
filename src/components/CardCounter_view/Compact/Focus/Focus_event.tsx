97c0109e9a2bc6ff3a4543cb446eed46bb604605
Azriel09 <jhanmarmel@gmail.com>
2025-09-22 12:02:13 +0800

    switch from card icons to event image (wide mode) and use reusable tab

--- src/components/CardCounter_view/Compact/Focus/Focus_event.tsx
+++ src/components/CardCounter_view/Compact/Focus/Focus_event.tsx
@@ -3,6 +3,8 @@ import { useProsekaData } from "../../../../context/Data";
 import CanvasTable from "../CanvasTable_container";
 import { useTheme } from "../../../../context/Theme_toggle";
 import { grouped, imgHost } from "../../../../constants/common";
+import { TabNavigation } from "../../../Nav/TabNav";
+
 type UnitKey = "mmj" | "wxs" | "l/n" | "n25" | "vbs";
 const unit_mapping: Record<UnitKey, string> = {
   mmj: "MORE MORE JUMP!",
@@ -13,7 +15,8 @@ const unit_mapping: Record<UnitKey, string> = {
 };
 
 export default function FocusEvent() {
-  const [eventRotation, setEventRotation] = useState(6);
+  const [currentTab, setCurrentTab] = useState(6);
+  const tabNames = ["1st", "2nd", "3rd", "4th", "5th", "6th"];
   const { jpEvents } = useProsekaData();
   const { theme } = useTheme();
   const focusEventGrid: [number, number] = [5, 5];
@@ -37,10 +40,10 @@ export default function FocusEvent() {
     );
     const match = findNumber?.match(/\d+/);
     const number = match ? parseInt(match[0], 10) : null;
-    return number === eventRotation;
+    return number === currentTab;
   });
   const dataPaths = filteredEventsByRotation.map((ev) => {
-    const iconPath = `${imgHost}/icons/${ev.cards[0]}_ut.webp`;
+    const iconPath = `${imgHost}/jp_events/${ev.id}.webp`;
     const convertedUnitName = unit_mapping[ev.unit as UnitKey];
     const whoseFocus = ev.type.split(" ");
     const name = whoseFocus.slice(0, -3).join(" ");
@@ -58,118 +61,25 @@ export default function FocusEvent() {
     });
     return { iconPath: iconPath, row: ro, col: co };
   });
+
   return (
     <div>
-      <div className="min-h-[70px]">
+      <div className="lg:min-h-[70px]]">
         <h1 className="text-center">Focus Events</h1>
         <div
           className={`max-w-[350px] flex mx-auto  border-b ${
             theme === "light" ? "border-gray-200" : "border-gray-700 "
           }`}
         >
-          <button
-            onClick={() => setEventRotation(1)}
-            className={`px-2 py-3 mr-2 w-1/6 text-sm font-medium relative transition-colors duration-200 ${
-              eventRotation === 1
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            1st
-            {eventRotation === 1 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-
-          <button
-            onClick={() => setEventRotation(2)}
-            className={`px-2 py-3 text-sm w-1/6 font-medium relative transition-colors duration-200 ${
-              eventRotation === 2
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            2nd
-            {eventRotation === 2 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-          <button
-            onClick={() => setEventRotation(3)}
-            className={`px-2 py-3 text-sm w-1/6 font-medium relative transition-colors duration-200 ${
-              eventRotation === 3
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            3rd
-            {eventRotation === 3 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-          <button
-            onClick={() => setEventRotation(4)}
-            className={`px-2 py-3 text-sm w-1/6 font-medium relative transition-colors duration-200 ${
-              eventRotation === 4
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            4th
-            {eventRotation === 4 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-          <button
-            onClick={() => setEventRotation(5)}
-            className={`px-2 py-3 text-sm w-1/6 font-medium relative transition-colors duration-200 ${
-              eventRotation === 5
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            5th
-            {eventRotation === 5 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
-          <button
-            onClick={() => setEventRotation(6)}
-            className={`px-2 py-3 text-sm w-1/6 font-medium relative transition-colors duration-200 ${
-              eventRotation === 6
-                ? theme === "light"
-                  ? "text-[#52649e]"
-                  : "text-[#6b85d6]"
-                : theme === "light"
-                ? "text-gray-500 hover:text-gray-700"
-                : "text-gray-400 hover:text-gray-200"
-            }`}
-          >
-            6th
-            {eventRotation === 6 && (
-              <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#52649e]"></div>
-            )}
-          </button>
+          {tabNames.map((tab, i) => (
+            <TabNavigation
+              currentTab={currentTab}
+              setCurrentTab={setCurrentTab}
+              tab={tab}
+              n={i + 1}
+              tabs={tabNames}
+            />
+          ))}
         </div>
       </div>
       <CanvasTable
@@ -178,6 +88,7 @@ export default function FocusEvent() {
         filename={"focus.png"}
         startAtRow2={false}
         dataPaths={dataPaths}
+        wide={true}
       />
     </div>
   );
